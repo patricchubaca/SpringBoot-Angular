4257ce75defc4593f684dd610dafdece58f1ae25
patricchubaca <patric_job@outlook.com>
2026-05-02 11:10:54 -0300

    Feat User :: Repository

--- LoginAuthAPI/src/main/java/com/example/HomeController.java
+++ LoginAuthAPI/src/main/java/com/example/HomeController.java
@@ -1,13 +1,14 @@
 package com.example.LoginAuthAPI.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "API rodando 🚀";
+    public String home(@RequestParam(value = "mensagem", defaultValue = "API rodando 🚀") String mensagem) {
+        return mensagem;
     }
 }
